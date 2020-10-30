8d847b11d8d0dcd140d54595d95ecdad243b0411
albertec1 <45201952+albertec1@users.noreply.github.com>
2020-10-30 19:38:52 +0100

    Devil loading correctly

--- My_Engine/Source Code/TextureImporter.cpp
+++ My_Engine/Source Code/TextureImporter.cpp
@@ -1,11 +1,17 @@
 #include "TextureImporter.h"
 #include "Globals.h"
-#include "Assimp.h"
 #include "Application.h"
-
-#pragma comment (lib, "assimp.lib")
+#include "Dependencies/Assimp/include/material.h"
+#include "Dependencies/Assimp/include/texture.h"
 
 #include <cmath>
+
+#pragma comment( lib, "Devil/libx86/DevIL.lib" )
+#include "Dependencies/Devil/include/ilu.h"
+#pragma comment( lib, "Devil/libx86/ILU.lib" )
+#include "Dependencies\Devil\include\ilut.h"
+#pragma comment( lib, "Devil/libx86/ILUT.lib" )
+
 //Devil
 
 TextureImporter::TextureImporter()
